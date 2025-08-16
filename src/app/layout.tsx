04b9018817c7ffa5c2be0f63d789b2ad776e8da6
Akram Shaikh <akram19488@gmail.com>
2025-08-16 08:57:44 +0000

    remove the hi fun from code - yapp name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'code-yapp',
+  title: 'codeyapp',
   description: 'Share and discuss code in real-time without logging in.',
 };
 
