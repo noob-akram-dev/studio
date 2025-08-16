7c0944915c6824c8b3099b0205a2113add9d7b7a
Akram Shaikh <akram19488@gmail.com>
2025-08-16 09:50:00 +0000

    the thing is as soon as the home screen appers it is scrollable up and d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home({
   const error = searchParams?.error;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 bg-background">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 bg-background overflow-x-hidden">
       <div className="text-center mb-10 sm:mb-12">
         <Logo className="justify-center" />
         <p className="text-muted-foreground mt-2 text-md sm:text-lg">
