ea32129014a03d4b0f717cf9563d15c4e10e60bb
Akram Shaikh <akram19488@gmail.com>
2025-08-16 19:25:39 +0000

    enable pwa so users can download it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster"
 export const metadata: Metadata = {
   title: 'Code Yapp',
   description: 'Yapp about your code. Code about your yap.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet"></link>
+        <meta name="theme-color" content="#27272a" />
       </head>
       <body className="font-body antialiased">
         {children}
