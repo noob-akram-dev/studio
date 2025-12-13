e00028fe1538ee17740873d07555e903fb40e88d
Akram Shaikh <akram19488@gmail.com>
2025-12-13 08:05:43 +0000

    Try fixing this error: `Console Error: ReplyError: ERR max number of cli

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,7 +4,8 @@ import Redis from 'ioredis';
 // This is a common pattern to cache the Redis connection in a serverless environment.
 // It prevents creating a new connection on every function invocation.
 declare global {
-  var redis: Redis | undefined;
+  var redisClient: Redis | undefined;
+  var redisSubClient: Redis | undefined;
 }
 
 const redisUrl = process.env.REDIS_URL;
@@ -13,42 +14,41 @@ if (!redisUrl) {
   throw new Error('REDIS_URL environment variable not set. Please configure your .env file.');
 }
 
-// Due to the nature of Redis Pub/Sub, we need a separate client for subscribing
-// because a client in subscriber mode cannot be used for other commands.
-let subClient: Redis | undefined;
-
-function getRedisClient(forSubscription = false): Redis {
-    if (forSubscription) {
-        if (!subClient) {
-            subClient = new Redis(redisUrl, {
-                lazyConnect: true,
-                maxRetriesPerRequest: 1,
-            });
-             subClient.on('error', (err) => {
-                console.error('Redis Subscription Client error:', err);
-                subClient = undefined;
-            });
-        }
-        return subClient;
-    }
-
-    if (global.redis) {
-        return global.redis;
-    }
-
+function createNewClient(): Redis {
     const client = new Redis(redisUrl, {
         lazyConnect: true,
         maxRetriesPerRequest: 1, // Don't retry on connection errors, fail fast.
     });
-
+    
+    // On error, we want to clear the cached client to force a reconnect on the next request.
     client.on('error', (err) => {
         console.error('Redis connection error:', err);
-        // On error, we want to clear the cached client to force a reconnect on the next request.
-        global.redis = undefined;
+        if (global.redisClient === client) {
+            global.redisClient = undefined;
+        }
+        if (global.redisSubClient === client) {
+            global.redisSubClient = undefined;
+        }
     });
-
-    global.redis = client;
+    
     return client;
+}
+
+
+function getRedisClient(forSubscription = false): Redis {
+    if (forSubscription) {
+        if (!global.redisSubClient) {
+            console.log('Creating new Redis subscription client...');
+            global.redisSubClient = createNewClient();
+        }
+        return global.redisSubClient;
+    }
+
+    if (!global.redisClient) {
+        console.log('Creating new Redis main client...');
+        global.redisClient = createNewClient();
+    }
+    return global.redisClient;
 }
 
 export default getRedisClient;
